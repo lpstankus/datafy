9909e2e94abc60112f440832f529e1b64fb68009
Lucas Stankus <lucas.p.stankus@gmail.com>
2024-07-31 17:36:19 -0300

    router: spotify: store user's top tracks results in db
    This commit includes the processing and insertion of the tracks
    themselves, the artists, the genres, and the relations between these
    tables in postgres.

--- src/server/api/routers/spotify/router.ts
+++ src/server/api/routers/spotify/router.ts
@@ -1,114 +1,180 @@
 import { z } from "zod";
-import { TRPCError } from "@trpc/server";
+import { SQL, sql } from "drizzle-orm";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-import { createSelectSchema } from "drizzle-zod";
-import { accounts } from "~/server/db/schema";
-const selectAccountSchema = createSelectSchema(accounts);
-type Account = z.infer<typeof selectAccountSchema>;
+import {
+  tracks,
+  artists,
+  trackArtists,
+  trackGenres,
+  artistGenres,
+  InsertTrack,
+  InsertArtist,
+  InsertTrackArtist,
+  InsertTrackGenre,
+  InsertArtistGenre,
+} from "~/server/db/schema";
 
-import { TrackObject } from "./extern_types";
-
+import { TrackObject, ArtistObject } from "./extern_types";
 import { fetchArtistsData, fetchTopTracks } from "./extern_api";
 
-const ITEM_TARG = 1;
+const ITEM_TARG = 10;
 
-type ArtistData = {
-  spotify_id: string;
-  name: string;
-  genres: string[];
-  popularity: number;
-  followers: number;
-};
+export const spotifyRouter = createTRPCRouter({
+  fetchTopTracks: publicProcedure
+    .input(z.object({ accessToken: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      let { accessToken } = input;
 
-type TrackData = {
-  spotify_id: string;
-  track_name: string;
-  explicit: boolean;
-  popularity: number;
-  album_name: string;
-  release_year: number;
-  genres: string[];
-  artists: string[];
-};
+      let track_data = await fetchTopTracks(accessToken, ITEM_TARG);
+      let { ins_tracks, track_artists } = processTracksData(track_data);
 
-function getArtistIds(data: TrackObject[]): string[] {
-  const artist_ids = new Set<string>();
-  for (const track of data) {
+      let artist_ids = filterArtistIds(track_artists);
+      let artist_data = await fetchArtistsData(accessToken, artist_ids);
+      let ins_artists = processArtistsData(artist_data);
+
+      let { artist_genres, track_genres } = processGenres(track_data, artist_data);
+
+      let tracks_promise = ctx.db
+        .insert(tracks)
+        .values(ins_tracks)
+        .onConflictDoUpdate({
+          target: tracks.trackId,
+          set: {
+            trackName: sql`excluded."trackName"`,
+            albumName: sql`excluded."albumName"`,
+            releaseYear: sql`excluded."releaseYear"`,
+            explicit: sql`excluded."explicit"`,
+            popularity: sql`excluded."popularity"`,
+          },
+        });
+
+      let artists_promise = ctx.db
+        .insert(artists)
+        .values(ins_artists)
+        .onConflictDoUpdate({
+          target: artists.artistId,
+          set: {
+            name: sql`excluded."name"`,
+            popularity: sql`excluded."popularity"`,
+            followers: sql`excluded."followers"`,
+          },
+        });
+
+      let artist_tracks_promise = ctx.db
+        .insert(trackArtists)
+        .values(track_artists)
+        .onConflictDoNothing();
+
+      let track_genres_promise = ctx.db
+        .insert(trackGenres)
+        .values(track_genres)
+        .onConflictDoNothing();
+
+      let artist_genres_promise = ctx.db
+        .insert(artistGenres)
+        .values(artist_genres)
+        .onConflictDoNothing();
+
+      await tracks_promise;
+      await artists_promise;
+      await artist_tracks_promise;
+      await track_genres_promise;
+      await artist_genres_promise;
+    }),
+});
+
+function processTracksData(track_data: TrackObject[]): {
+  ins_tracks: InsertTrack[];
+  track_artists: InsertTrackArtist[];
+} {
+  let ins_tracks: InsertTrack[] = [];
+  let track_artists: InsertTrackArtist[] = [];
+
+  for (let track of track_data) {
     if (!track.id) continue;
-    for (const artist of track.artists || []) {
-      if (artist.id) artist_ids.add(artist.id);
+
+    for (let artist of track.artists || []) {
+      if (!artist.id) continue;
+      track_artists.push({
+        trackId: track.id,
+        artistId: artist.id,
+      });
     }
+
+    ins_tracks.push({
+      trackId: track.id,
+      trackName: track.name || "Untitled",
+      albumName: track.album?.name || "Untitled",
+      releaseYear: parseInt(track.album?.release_date.split("-")[0] || "0"),
+      explicit: track.explicit || false,
+      popularity: track.popularity || 0,
+    });
   }
-  return Array.from(artist_ids.values());
+
+  return { ins_tracks, track_artists };
 }
 
-async function transformSpotifyResponse(
-  accessToken: string,
-  track_data: TrackObject[],
-): Promise<{
-  artist_data: ArtistData[];
-  track_data: TrackData[];
-}> {
-  var artist_map = new Map<string, ArtistData>();
-  var track_map = new Map<string, TrackData>();
+function filterArtistIds(track_artists: InsertTrackArtist[]): string[] {
+  return [...track_artists.reduce((acc, obj) => acc.add(obj.artistId), new Set<string>())];
+}
 
-  const artist_ids = getArtistIds(track_data);
-  const artist_data = await fetchArtistsData(accessToken, artist_ids);
-
-  for (const artist of artist_data) {
+function processArtistsData(artist_data: ArtistObject[]): InsertArtist[] {
+  let processed_artists: InsertArtist[] = [];
+  for (let artist of artist_data) {
     if (!artist.id) continue;
-
-    const artist_obj: ArtistData = {
-      spotify_id: artist.id,
+    let artist_obj: InsertArtist = {
+      artistId: artist.id,
       name: artist.name || "Unknown",
-      genres: artist.genres || [],
       popularity: artist.popularity || 0,
       followers: artist.followers?.total || 0,
     };
-
-    artist_map.set(artist_obj.spotify_id, artist_obj);
+    processed_artists.push(artist_obj);
   }
-
-  for (const track of track_data) {
-    if (!track.id) continue;
-
-    var track_artist_ids: string[] = [];
-
-    var genres_set = new Set<string>();
-    for (const artist of track.artists || []) {
-      if (!artist.id) continue;
-      track_artist_ids.push(artist.id);
-      const genres = artist_map.get(artist.id)?.genres;
-      for (const genre of genres || []) genres_set.add(genre);
-    }
-
-    const track_obj: TrackData = {
-      track_name: track.name || "Untitled",
-      spotify_id: track.id,
-      album_name: track.album?.name || "Untitled",
-      release_year: parseInt(track.album?.release_date.split("-")[0] || "0"),
-      artists: track_artist_ids,
-      explicit: track.explicit || false,
-      popularity: track.popularity || 0,
-      genres: Array.from(genres_set),
-    };
-
-    track_map.set(track_obj.spotify_id, track_obj);
-  }
-
-  return {
-    artist_data: Array.from(artist_map.values()),
-    track_data: Array.from(track_map.values()),
-  };
+  return processed_artists;
 }
 
-export const spotifyRouter = createTRPCRouter({
-  retrieveMostPlayed: publicProcedure.query(async ({ ctx }) => {
-    let accessToken = ctx.session?.user.spotify.accessToken;
-    if (!accessToken) return null;
+function processGenres(
+  track_data: TrackObject[],
+  artist_data: ArtistObject[],
+): { track_genres: InsertTrackGenre[]; artist_genres: InsertArtistGenre[] } {
+  let artist_genres_map = new Map<string, Set<string>>();
+  for (let artist of artist_data) {
+    if (!artist.id) continue;
+    artist_genres_map.set(artist.id, new Set(artist.genres || []));
+  }
 
-    const track_data = await fetchTopTracks(accessToken, ITEM_TARG);
-    return await transformSpotifyResponse(accessToken, track_data);
-  }),
-});
+  let track_genres: InsertTrackGenre[] = [];
+  for (let track of track_data) {
+    if (!track.id) continue;
+    let genres_set = new Set<string>();
+
+    for (let artist of track.artists || []) {
+      if (!artist.id) continue;
+      let artist_genres = artist_genres_map.get(artist.id);
+      if (artist_genres) artist_genres.forEach((genre) => genres_set.add(genre));
+    }
+
+    for (let genre of genres_set) {
+      let track_genre: InsertTrackGenre = {
+        trackId: track.id,
+        genre,
+      };
+      track_genres.push(track_genre);
+    }
+  }
+
+  let artist_genres: InsertArtistGenre[] = [];
+  for (let pair of artist_genres_map) {
+    let [id, genre_set] = pair;
+    for (let genre of genre_set) {
+      let artist_genre: InsertArtistGenre = {
+        artistId: id,
+        genre,
+      };
+      artist_genres.push(artist_genre);
+    }
+  }
+
+  return { artist_genres, track_genres };
+}
