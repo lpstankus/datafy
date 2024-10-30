a561a6674e335104c14e0489214e609f2f9b787a
Lucas Stankus <lucas.p.stankus@gmail.com>
2024-10-30 12:35:07 -0300

    cron: don't do sequential snapshots when we can do "parallel"

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -9,7 +9,9 @@ export async function GET(request: Request) {
 
   let idList = env["TRACKED_USERS_IDS"].split(",").filter((str) => str.length > 0);
   try {
-    for (let userId of idList) await api.spotify.snapshotUser({ userId });
+    let promises: Promise<void>[] = [];
+    for (let userId of idList) promises.push(api.spotify.snapshotUser({ userId }));
+    await Promise.all(promises);
     return NextResponse.json({ body: "Exited successfully!" });
   } catch (e) {
     return NextResponse.json({ body: e }, { status: 500 });
